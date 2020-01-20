158fdcbeb1c58b2989edcd323d574ef62cf2c9a5
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-20 07:09:39 -0800

    Theater done

--- inheritance/src/main/java/inheritance/Theater.java
+++ inheritance/src/main/java/inheritance/Theater.java
@@ -1,6 +1,7 @@
 package inheritance;
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,5 +13,37 @@ public class Theater {
 
     LinkedList<Review> theaterReviews;
 
-    Theater(String name, int rating)
+    Theater(String name, int rating) {
+        this.name = name;
+        this.movies = new ArrayList<>();
+        this.rating = rating;
+        this.numberOfReviews = 1;
+        this.theaterReviews = new LinkedList<>();
+    }
+
+    public void addMovie (String movie) {
+        this.movies.add(movie);
+    }
+
+    public void removeMovie (String movie) {
+        this.movies.remove(new String(movie));
+    }
+
+    public String toString() {
+        String theaterDetails = this.name + "theater is showing ";
+        for (int i = 0; i < movies.size(); i++) {
+            if (i == movies.size() - 1) {
+                theaterDetails += "and " + movies.get(i) + ".";
+            } else {
+                theaterDetails += movies.get(i) + ", ";
+            }
+        }
+        return theaterDetails;
+    }
+
+    public  void addReview (Review.newReview) {
+        this.numberOfReviews ++;
+        this.rating = this.rating + newReview.rating/this.numberOfReviews;
+        newReview.theaterReviews.addFirst(newReview);
+    }
 }
